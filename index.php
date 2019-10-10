83dc5f04a9196cc1bc05330b1b5158d252dc2763
Piloni <gabriel.piloni@hotmail.com>
2019-10-09 23:15:24 -0300

    Modificação no Arquivo index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-    phpinfo();
-?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exemplo Deploy Automatico na Umbler</title>
+</head>
+<body>
+<h1>Deu Certooo!</h1>
+
+    <?php
+        phpinfo();
+    ?>
+</body>
+</html>
+
